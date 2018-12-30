02fd734ea603e72b5046cdec931eab7e6cd8f6d6
Mike Airey <m.airey@ract.com.au>
2018-12-31 10:36:21 +1100

    Initial commit.

---
+++ NyeCountdown.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class NyeCountdown
+    {
+        public TimeSpan GetTimeUntilNye()
+        {
+            var newYear = new DateTime(2019, 1, 1);
+            var untilNewYear = newYear - DateTime.Now;
+            return untilNewYear;
+        }
+    }
+}
