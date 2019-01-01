59f49428334092df9f6403557e068e59ab643884
Mike Airey <m.airey@ract.com.au>
2019-01-02 08:57:25 +1100

    Moved test project into this repo.

---
+++ HelloWorld.Test/NyeCountdownTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace HelloWorld.Test
+{
+    [TestFixture]
+    public class NyeCountdownTests
+    {
+        [Test]
+        public void TimeUntilNyeShouldNotBeNegative()
+        {
+            var sut = new NyeCountdown();
+            var result = sut.GetTimeUntilNye();
+            Assert.That(result.TotalSeconds, Is.GreaterThanOrEqualTo(0), nameof(result.TotalSeconds));
+        }
+    }
+}
